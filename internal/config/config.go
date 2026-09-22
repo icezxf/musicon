dd09665d58de264662d288beafcd32cb228a297b
icezxf <122969420+icezxf@users.noreply.github.com>
2026-09-22 10:19:45 +0800

    Refactor Config struct and Load function

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,34 +3,28 @@ package config
 import "os"
 
 type Config struct {
-Listen      string
-DBPath      string
-DataDir     string
-StaticDir   string
-AListURL    string
-AListToken  string
-LXServerURL string
-WebUser     string
-WebPass     string
+	Listen    string
+	DBPath    string
+	DataDir   string
+	StaticDir string
+	WebUser   string
+	WebPass   string
 }
 
 func Load() *Config {
-return &Config{
-Listen:      env("LISTEN", "0.0.0.0:8000"),
-DBPath:      env("DB_PATH", "./data/musicon.db"),
-DataDir:     env("DATA_DIR", "./data"),
-StaticDir:   env("STATIC_DIR", "./static"),
-AListURL:    env("AListURL", ""),
-AListToken:  env("AListToken", ""),
-LXServerURL: env("LXServerURL", "http://127.0.0.1:9527"),
-WebUser:     env("WEB_USER", "fire"),
-WebPass:     env("WEB_PASS", "boc81838"),
-}
+	return &Config{
+		Listen:    env("LISTEN", "0.0.0.0:8000"),
+		DBPath:    env("DB_PATH", "./data/musicon.db"),
+		DataDir:   env("DATA_DIR", "./data"),
+		StaticDir: env("STATIC_DIR", "./static"),
+		WebUser:   env("WEB_USER", "admin"),
+		WebPass:   env("WEB_PASS", "admin"),
+	}
 }
 
 func env(k, def string) string {
-if v := os.Getenv(k); v != "" {
-return v
-}
-return def
+	if v := os.Getenv(k); v != "" {
+		return v
+	}
+	return def
 }
